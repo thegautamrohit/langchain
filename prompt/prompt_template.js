56c1cd7c5b5d0f542533bc99a064442d4d19ecab
Rohit <rgautam1997@gmail.com>
2025-11-16 14:19:23 +0530

    promppt template

--- prompt/prompt_template.js
+++ prompt/prompt_template.js
@@ -12,14 +12,18 @@ const promptTemplate = PromptTemplate.fromTemplate(
     "Explain {userInput} in simple terms. Provide only the explanation without showing your thinking process."
 );
 
-// Create a chain by piping the prompt to the LLM
-const chain = promptTemplate.pipe(LLM);
 
-const userInput = 'pythagoras theorem';
-
-// Invoke the chain with the variable values
-const response = await chain.invoke({
-    userInput: userInput,
+const prompt = await promptTemplate.invoke({
+    userInput: 'pythagoras theorem',
 });
 
+console.log(prompt);
+
+const response = await LLM.invoke(prompt);
+
 console.log(response);
+
+// This is how we can use the prompt template to generate a prompt for the LLM
+// The prompt can have multiple variables and we can invoke the prompt template to generate a prompt for the LLM
+// Creating the prompt template is a good way to avoid hardcoding the prompt in the code and it also provides a better way
+// to manage and safe check the prompt for execution
